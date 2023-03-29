cf5e9f130c9d327a485f8eb6852fedf0b51d929e
walwizard <goneisgone@hotmail.com>
2023-03-29 20:04:12 -0300

    Wizard Chess - Initial SEtup

---
+++ wizard-chess/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <HOME>
+
+    </HOME>
+  )
+}
+
+const HOME = styled.div `
+
+`
+
+export default App;
